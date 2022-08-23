fe419aee7b29a87d9c1e01dd87a5c6156b30d953
Kang Dong Hyeon <mathdong01@gmail.com>
2022-08-23 21:46:45 +0900

    Update render texture when screen resized

--- Assets/Scripts/ScreenCamera.cs
+++ Assets/Scripts/ScreenCamera.cs
@@ -25,6 +25,9 @@ public class ScreenCamera : MonoBehaviour
 
     public Material blitMaterial;
 
+    GameObject quad;
+    Material mat;
+
     // @Todo: Update if screen resolution is changed.
     public static RenderTexture CreateRenderTexture(Camera camera, float downscale, RenderTextureFormat format = RenderTextureFormat.ARGBFloat)
     {
@@ -41,19 +44,39 @@ public class ScreenCamera : MonoBehaviour
 
         renderTexture = CreateRenderTexture(cam, downscale);
 
-        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
 
         quad.transform.parent = transform;
         quad.transform.localPosition = new Vector3(0, 0, 1);
         quad.transform.localRotation = Quaternion.identity;
         quad.transform.localScale = new Vector3(cam.orthographicSize * cam.aspect * 2, cam.orthographicSize * 2, 1);
 
-        Material mat = Instantiate(blitMaterial);
+        mat = Instantiate(blitMaterial);
         mat.SetTexture("_MainTex", renderTexture);
 
         quad.GetComponent<MeshRenderer>().sharedMaterial = mat;
 
         gameCamera.targetTexture = renderTexture;
         // gameCamera.depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+
+    void Update() {
+        Vector2Int targetResolution = new Vector2Int(Mathf.RoundToInt(cam.pixelWidth / downscale), Mathf.RoundToInt(cam.pixelHeight / downscale));
+
+        if (targetResolution.x != renderTexture.width || targetResolution.y != renderTexture.height) {
+            Debug.Log("Screen resolution updated.");
+
+            gameCamera.targetTexture = null;
+
+            renderTexture.Release();
+
+            renderTexture = CreateRenderTexture(cam, downscale);
+
+            gameCamera.targetTexture = renderTexture;
+
+            quad.transform.localScale = new Vector3(cam.orthographicSize * cam.aspect * 2, cam.orthographicSize * 2, 1);
+
+            mat.SetTexture("_MainTex", renderTexture);
+        }
     }
 }
